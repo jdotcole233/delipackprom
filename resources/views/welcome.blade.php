89ec66ec53bc7f54ca23a85c8f88a7ba3f504f70
jdotcole233 <jdotcole233@gmail.com>
2019-05-22 02:46:14 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,368 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<link rel="apple-touch-icon" sizes="180x180" href="images/assets/favicon_io/apple-touch-icon.png">
+
+<link rel="icon" type="image/png" sizes="32x32" href="images/assets/favicon_io/favicon-32x32.png">
+
+<link rel="icon" type="image/png" sizes="16x16" href="images/assets/favicon_io/favicon-16x16.png">
+<title>Package Delivery - DeliPack</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="keywords" content="Become your own boss, delipack, DeliPack, delivpack, DelivPack, Quick pick up and delivery,
+courier service, Ghana motor riders delivery, courier companies, best delivery platform" />
+
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+	
+	<!-- css files -->
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' /><!-- bootstrap css -->
+    <link href="css/style.css" rel='stylesheet' type='text/css' /><!-- custom css -->
+    <link href="css/font-awesome.min.css" rel="stylesheet"><!-- fontawesome css -->
+	<!-- //css files -->
+	
+	<!-- google fonts -->
+	<link href="http://fonts.googleapis.com/css?family=Rosario:400,400i,700,700i" rel="stylesheet">
+	<!-- //google fonts -->
+	
+</head>
+<body>
+<script src='../../../../../../../ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script><script src="../../../../../../../m.servedby-buysellads.com/monetization.js" type="text/javascript"></script>
+<script>
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+  		// format, zoneKey, segment:value, options
+  		_bsa.init('flexbar', 'CKYI627U', 'placement:w3layoutscom');
+  	}
+})();
+</script>
+<script>
+(function(){
+if(typeof _bsa !== 'undefined' && _bsa) {
+	// format, zoneKey, segment:value, options
+	_bsa.init('fancybar', 'CKYDL2JN', 'placement:demo');
+}
+})();
+</script>
+<script>
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+  		// format, zoneKey, segment:value, options
+  		_bsa.init('stickybox', 'CKYI653J', 'placement:w3layoutscom');
+  	}
+})();
+</script>
+<script type="text/javascript" src="http://services.bilsyndication.com/adv1/?d=353" defer="" async=""></script><script> var vitag = vitag || {};vitag.gdprShowConsentTool=false;vitag.outStreamConfig = {type: "slider", position: "left" };</script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125810435-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-125810435-1');
+</script><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','../../../../../../../www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-30027142-1', 'w3layouts.com');
+  ga('send', 'pageview');
+</script>
+<body>
+<!-- //header -->
+<header class="delipacknav">
+
+
+	<div class="container ">
+        <!-- nav -->
+		<nav class="py-sm-4 py-3 d-lg-flex">
+			<div id="logo">
+				<h1> <a href="index.html"><span>
+                    <img src="images/assets/deli_s.png" alt="Delivery Package Logo" width="10%" height="10%" >
+                </span> DeliPack</a></h1>
+			</div>
+			<label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
+			<input type="checkbox" id="drop" />
+			<ul class="menu mt-md-2 ml-auto">
+				<li class="mr-lg-4 mr-2 active"><a href="index.html">Home</a></li>
+				<li class="mr-lg-4 mr-2"><a href="#about">About Us</a></li>
+				<li class="mr-lg-4 mr-2"><a href="#services">Services</a></li>
+				<li class="mr-lg-4 mr-2"><a href="#tarrifs">Our Partners</a></li>
+				<li class="mr-lg-4 mr-2"><a href="#stats">Contact Us</a></li>
+				<!-- <li class="mr-lg-4 mr-2"><a href="#app">App Download</a></li> -->
+			</ul>
+		</nav>
+		<!-- //nav -->
+	</div>
+</header>
+<!-- //header -->
+		
+<!-- banner -->
+<section class="banner w3pvt-banner" id="home">
+	<div class="container">
+		<div class="banner-text">
+			<div class="slider-info">
+				<div class="w3pvt-logo">
+					<h2>Connecting you to over 1000 customers around you</h2>
+					<!-- <h3>+1(12) 366 411 4999</h3> -->
+					<p class="mt-3"> Always Available </p>
+					<!-- <img src="images/delivery-boy.png" alt="" width="30%" class="img-fluid mt-md-2"/> -->
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //banner -->
+<!---728x90--->
+
+<!-- about -->
+<section class="about py-sm-5 py-4" id="about">
+	<div class="container py-lg-5">
+		<div class="row about-grids">
+			<div class="col-lg-5">
+				<h6 class="mt-4">Be your own boss</h6>
+				<h4 class="mt-3">Become a trusted delivery service in  Your City With DeliPack</h4>
+				<p class="mt-sm-4 mt-3">We are providing you with a vibrant and state of the art platform to make your product/service readily available to a wider audience. No need to worry about advertisement. DeliPack is aimed at making your company known to every busy/idle person in Ghana.</p>
+				<p class="mt-sm-4 mt-3">Why worry about paper work, when delipack is providing you with a safe, secure and easy to use platform. In just some few simple steps, delipack is giving you the power to control your business from the comfort of your home or office.</p>
+			</div>
+			<div class="col-lg-4 col-md-6 mt-lg-0 mt-4">
+				<div class="padding">
+					<form action="#" method="post">
+						<h5 class="mb-3">Request our service</h5>
+						<div class="form-style-agile">
+							<input placeholder="Name" name="name" type="text" required="">
+							<input placeholder="Phone Number" name="number" type="text" required="">
+							<input placeholder="Source(From)" type="text" required="">
+							<input placeholder="Destination(To)" type="text" required="">
+							<select>
+							  <option value="0">Choose Vehicle</option>
+							  <option value="1">Vehicle1</option>
+							  <option value="2">Vehicle2</option>
+							  <option value="3">Vehicle3</option>
+							  <option value="3">Vehicle4</option>
+							  <option value="3">Vehicle5</option>
+							</select>
+							<!--<input placeholder="Password" name="password" type="password" required=""> -->
+							<button class="book-btn btn">Book Now</button>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-6 mt-4">
+				<img src="images/about.jpg" alt="" class="img-fluid"/>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //about -->
+<!---728x90--->
+
+<!-- //what we do -->
+<section class="services py-5" id="services">
+	<div class="container py-lg-5 py-3">
+		<div class="row service-grid-grids text-center">
+			<div class="col-lg-4 col-md-6 service-grid service-grid1">
+				<div class="service-icon">
+					<span class="fa fa-motorcycle"></span>
+				</div>
+				<h4 class="mt-3">Fast and Safe</h4>
+				<p class="mt-3">Delivery is fast if your pick up is around you. Spare your customers no waiting time.</p>
+			</div>
+			<div class="col-lg-4 col-md-6 service-grid service-grid2 mt-md-0 mt-5">
+				<div class="service-icon">
+					<span class="fa fa-user"></span>
+				</div>
+				<h4 class="mt-3">Experienced Riders</h4>
+				<p class="mt-3">We bring the customers to your business, you handle the elite riders for their need.</p>
+			</div>
+			
+			<div class="col-lg-4 col-md-6 service-grid service-grid3 mt-lg-0 mt-5">
+				<div class="service-icon">
+					<span class="fa fa-mobile"></span>
+				</div>
+				<h4 class="mt-3">Quick Search</h4>
+				<p class="mt-3"> Your business can be located whenever, where-ever. Grow you business.</p>
+			</div>
+			<div class="col-lg-4 col-md-6 service-grid service-grid4 mt-5">
+				<div class="service-icon">
+					<span class="fa fa-lock"></span>
+				</div>
+				<h4 class="mt-3">Secured Business</h4>
+				<p class="mt-3">Keep track of your rider's activities and don't worry about sales record.</p>
+			</div>
+			<div class="col-lg-4 col-md-6 service-grid service-grid4 mt-5">
+				<div class="service-icon">
+					<span class="fa fa-file"></span>
+				</div>
+				<h4 class="mt-3">Accountability</h4>
+				<p class="mt-3">Be accountable for your customers product and they will know they can trust you.</p>
+			</div>
+			<div class="col-lg-4 col-md-6 service-grid service-grid6 mt-5">
+				<div class="service-icon">
+					<span class="fa fa-cogs"></span>
+				</div>
+				<h4 class="mt-3">Business Growth</h4>
+				<p class="mt-3">Business is important to us, as it is to you. Keep servicing your clients to be in service</p>
+			</div>
+		</div>
+	</div>		
+</section>
+<!-- what we do -->
+<!---728x90--->
+
+<!-- tarrifs -->
+<!-- <section class="tarrifs py-5" id="tarrifs"> -->
+<!-- <div class="container py-md-5">
+		<div class="row tarrifs-grid">
+			<div class="col-lg-4 col-md-6 text-center">
+				<img src="images/taxi1.jpg" alt="" class="img-fluid"/>
+				<div class="tarrif-info">
+					<h4 class="my-3">Economy Class</h4>
+					<p class="para_vl">Nam arcu mauris, tincidunt sed convallis non, egestas ut lacus. Cras sapien urna, malesuada ut varius.</p>
+					<h5 class="mt-sm-3 mt-2">$16/Km</h5>
+				</div>		
+			</div>		
+			<div class="col-lg-4 col-md-6 text-center">
+				<img src="images/taxi2.jpg" alt="" class="img-fluid"/>
+				<div class="tarrif-info">
+					<h4 class="my-3">Standard Class</h4>
+					<p class="para_vl">Nam arcu mauris, tincidunt sed convallis non, egestas ut lacus. Cras sapien urna, malesuada ut varius.</p>
+					<h5 class="mt-sm-3 mt-2">$20/Km</h5>
+				</div>		
+			</div>		
+			<div class="col-lg-4 col-md-6 text-center mt-lg-0 mt-5">
+				<img src="images/taxi1.jpg" alt="" class="img-fluid"/>
+				<div class="tarrif-info">
+					<h4 class="my-3">Business Class</h4>
+					<p class="para_vl">Nam arcu mauris, tincidunt sed convallis non, egestas ut lacus. Cras sapien urna, malesuada ut varius.</p>
+					<h5 class="mt-sm-3 mt-2">$25/Km</h5>
+				</div>		
+			</div>		
+		</div>		
+	</div>		 -->
+<!-- </section> -->
+<!-- //tarrifs -->
+
+<!-- stats -->
+<section class="stats py-5" id="stats">
+	<div class="container py-sm-3">	
+		<div class="row">
+			<div class="col-sm-4 col-6 text-center">
+				<span class="fa mr-1 fa-users"></span>
+				<h4>10,000+</h4>
+				<p>Customers</p>
+			</div>	
+			<div class="col-sm-4 col-6 text-center">
+				<span class="fa mr-1 fa-motorcycle"></span>
+				<h4>1200</h4>
+				<p>Elite riders</p>
+			</div>	
+			<div class="col-sm-4 col-6 mt-sm-0 mt-4 text-center">
+				<span class="fa mr-1 fa-map"></span>
+				<h4>500+</h4>
+				<p>Delivery Routes</p>
+			</div>		
+		</div>		
+	</div>		
+</section>
+<!-- //stats -->
+
+<!-- call us -->
+<section class="customer-call">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 py-sm-5 py-4 mt-3">
+				<h4 class="mt-2">24 Hours 7 Days a Week</h4>
+				<p>Call Us Now</p>
+				<h2><span class="fa mr-1 fa-phone"></span> +233 503 848404</h2>
+				<p class="mt-4">Setting up your business with DeliPack is quick and easy. You manage your own business without intrusion.</p>
+			</div>		
+			<div class="col-lg-3 offset-lg-1 col-md-4 col-sm-6 col-8 pt-md-5 mt-lg-3">
+				<img src="images/agent-18762.png" alt="">
+			</div>		
+		</div>		
+	</div>		
+</section>
+<!-- //call us -->
+
+<!--/app-->
+<section class="mobile-app-sec py-5" id="app">
+	<div class="container py-sm-3">
+		<!--/mobile-app -->
+		<div class="row inner-sec-wthree-agileits">
+			<div class="col-lg-7 pt-lg-5 app-info text-left">
+				<h3 class="tittle mob pb-sm-4">Download the Mobile App for free <br>Get More Exciting Offers</h3>
+				<p class="para_vl">Nam arcu mauris, tincidunt sed convallis non, egestas ut lacus. Cras sapien urna, malesuada ut varius consequat, hendrerit nisl. Aliquam vestibulum, odio non ullamcorper malesuada totam rem aperiam, eaque ipsa quae.</p>
+				<div class="app-devices">
+					<a href="#" class="mr-3">
+						<img src="images/app.png" alt="">
+					</a>
+					<a href="#">
+						<img src="images/app1.png" alt="">
+					</a>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="col-lg-3 offset-lg-2 col-md-4 col-sm-6 col-8 app-img text-right">
+				<img src="images/mobile.png" alt=" " class="img-fluid">
+			</div>
+			<!--//mobile-app -->
+		</div>
+	</div>
+</section>
+<!--//app-->
+
+<!-- footer -->
+<footer class="py-5">
+	<div class="container">
+		<div class="row footer_grids pt-lg-3">
+			<div class="col-lg-5 footer_left">
+				<h3 class="mb-sm-4 mb-3">About DeliPack</h3>
+				<p>Nulla felis tortor, rutrum eget feugiat enon, blandit tellus. Nam pharetra ipsum ligula volutpat, a finibus.
+				Fuscet sed malesuada sollicitudin venenatis. Aenean aculis faucibus iaculis. Donec finibus est sit amet mi ultrices,
+				id fringilla mauris efficitur. In eu enim quis nulla iaculis tempus.</p>
+			</div>
+			<div class="col-lg-3 col-md-6 mt-lg-0 mt-sm-5 mt-4 footer_right">
+				<h3 class="mb-sm-4 mb-3">Contact Us</h3>
+				<p> <span class="fa mr-1 fa-map-marker"></span> Hse No. 11, Agbogba. Accra- Ghana </p>
+				<p> <span class="fa mr-1 fa-envelope"></span> <a href="mailto:info@example.com">support@delivpack.com</a> </p>
+				<p> <span class="fa mr-1 fa-phone"></span> +233 503 848404 </p>
+			</div>
+			<div class="col-lg-4 col-md-6 mt-lg-0 mt-sm-5 mt-4 footer_middle">
+				<h3 class="mb-sm-4 mb-3">Download Apps</h3>
+				<p>Nulla felis tortor, rutrum eget feugiat enon, blandit tellus. Nam pharetra ipsum ligula volutpat, a finibus sit amet mi ultrices.</p>
+				<ul class="mt-3">
+					<li class="mr-3"><a href="#">Android Users <span class="fa ml-1 fa-android"></span></a></li>
+					<li><a href="#">IOS Users <span class="fa ml-1 fa-apple"></span></a></li>
+				</ul>
+			</div>
+		</div>
+		<!-- move top -->
+		<div class="move-to-top text-center mt-3">
+			<a href="#home" class="move-top"><span class="fa fa-angle-double-up" aria-hidden="true"></span></a>
+		</div>
+		<!-- //move top -->
+		<div class="copyright text-center mt-3">
+			<p>© 2019 Taxi Drive. All Rights Reserved | Design by <a href="http://w3layouts.com/"> W3layouts</a> </p>
+		</div>
+	</div>
+</footer>
+<!-- //footer -->
+
+</body>
+
+<!-- Mirrored from demo.w3layouts.com/demos_new/template_demo/02-02-2019/taxi_drive-demo_Free/641760209/web/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 16 May 2019 13:49:20 GMT -->
+</html>
