5f16632e79fa59e19a0667173981ec17cac3de39
Gh0stM0de <=jdotcole233@gmail.com>
2019-05-22 18:00:54 +0000

    about us content

--- resources/views/officialpages/about.blade.php
+++ resources/views/officialpages/about.blade.php
@@ -3,15 +3,21 @@
 
 <!-- Introduction -->
 <section class="introsection">
-    <div></div>
+    <div>
+        <img src="{{asset('images/values.png')}}" alt="">
+    </div>
     <div>
         <h1>Our Aim</h1> <br>
         <p>We are exposed to exciting opportunities if we can get the most done in as little time as possible. Whatever your Business is  (home, office or hanging out) do more by delegating some of your errands to Ghana’s most reliable riders. The world today is fueled by amazing cutting edge technologies. DeliPack has seen the gap between customers and delivery companies, therefore Delipack is giving you the CUSTOMER the opportunity to reach several elite riders who work around the clock to get your services satisfied with little or no overhead of stress. With just a few taps on your phone, you can find the closest reliable delivery guy to serve your purpose.
         </p> <br>
+    </div>
+    
+</section>
+<section class="introsecondsection">
+    <div>
         <p>As we say “ Do what you do so well that they will want to see it again and bring their friends “. To delivery companies, accountability is of the essence to customers. Our research has shown that customer is willing to trust their services to riders if they don’t have to deal with individual riders. Building a strong brand leaves a mark with all your customers and in return draws in lots of profit to your organization.  Delipack provides you with a platform to manage activities in your company and also communicate with a wider audience who would want to procure your service. We leave the details of your experience after you join Delipack. 
         Our aim is to bridge the gap between you and your customers.</p> 
     </div>
-    
 </section>
 
 <!-- Why us -->
@@ -33,7 +39,9 @@
 
 <!-- Dedicated team -->
 <section class="dedicatesection">
-    <div></div>
+    <div>
+        <img src="{{asset('images/dedicated_team.png')}}" width="50%" alt="">
+    </div>
     <div>
         <h1>Dedicated team</h1>
         <p>In relation to our aim to bring your business to the customers and make life easier for them, we have a dedicated team of marketers and developers who work around the clock to ensure your business is made available to every household in Ghana. </p>
