af7ca547f621cc6cbdad42a5acfdce64f1c4a7f0
Gh0stM0de <=jdotcole233@gmail.com>
2019-07-29 19:24:07 +0000

    partners js progress

---
+++ public/js/delipartners.js
@@ -0,0 +1,58 @@
+$(document).ready(function (){
+
+
+if (location.pathname != '/') {
+    $('#contactnav').attr('href', '/');
+    console.log($('#contactnav').attr('href'));
+}
+
+
+companyNames();
+
+function companyNames() {
+    let logoDisplays = "";
+    $('.partnerslogsdisplay').html("");
+
+    $.ajax({
+        method: 'GET',
+        url: 'http://delipackport.com/api/getcompanylist',
+        success: function (data) {
+            $.each(data, function (i, v) {
+                if (v.company_logo_path != 'delipack1.png') {
+                    let logoDisplay = `<div> <img src="http://superuser.delipackport.com/company_logos/${v.company_logo_path}" /></div>`;
+                    logoDisplays = `<div class="slide"> <img width="45px" src="http://superuser.delipackport.com/company_logos/${v.company_logo_path}" /></div>`;
+                    $('.partnerslogsdisplay').append(logoDisplay);
+                    // $('.slick-track').append(logoDisplays);
+                    $('.customer-logos').slick('slickAdd', logoDisplays)
+
+                }
+            });
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+
+    $('.customer-logos').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+}
+
+});
