34ab742ec1cf1d81efa908789909bf6dc367970c
Gh0stM0de <=jdotcole233@gmail.com>
2019-07-29 22:09:35 +0000

    parteners changes 3 ssl 8.6

--- public/js/slidedeli.js
+++ public/js/slidedeli.js
@@ -1,30 +1,27 @@
 $(document).ready(function () {
-
-
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
     if (location.pathname != '/') {
         $('#contactnav').attr('href', '/');
         console.log($('#contactnav').attr('href'));
     }
-    
-      $('.customer-logos').slick({
-          slidesToShow: 6,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 1000,
-          arrows: false,
-          dots: false,
-          pauseOnHover: false,
-          responsive: [{
-              breakpoint: 768,
-              settings: {
-                  slidesToShow: 4
-              }
-          }, {
-              breakpoint: 520,
-              settings: {
-                  slidesToShow: 3
-              }
-          }]
-      });
 
 });
