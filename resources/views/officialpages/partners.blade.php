86709366bba6dc8a90c5ff75ced66a452f14bc24
Gh0stM0de <=jdotcole233@gmail.com>
2019-07-29 21:57:48 +0000

    parteners changes 3 ssl 8.3

--- resources/views/officialpages/partners.blade.php
+++ resources/views/officialpages/partners.blade.php
@@ -12,7 +12,13 @@
     </section>
 
     <section class="partnerslogsdisplay">
-            
+            @foreach ($company_names as $company_name)
+                @if($company_name->company_logo_path != "delipack1.png")
+                    <div> 
+                        <img src="https://superuser.delipackport.com/company_logos/{{$company_name->company_logo_path}}" />
+                    </div>
+                @endif
+            @endforeach
     </section>
 </section>
 
