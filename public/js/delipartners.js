cb2a049f5ee07a15484e4f83aa49f48972f0a4af
Gh0stM0de <=jdotcole233@gmail.com>
2019-07-29 20:36:27 +0000

    parteners changes 3 ssl 3

--- public/js/delipartners.js
+++ public/js/delipartners.js
@@ -33,26 +33,26 @@ if (location.pathname != '/') {
 //         }
 //     });
 
-    $('.customer-logos').slick({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-}
+//     $('.customer-logos').slick({
+//         slidesToShow: 5,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 1500,
+//         arrows: false,
+//         dots: false,
+//         pauseOnHover: false,
+//         responsive: [{
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 4
+//             }
+//         }, {
+//             breakpoint: 520,
+//             settings: {
+//                 slidesToShow: 3
+//             }
+//         }]
+//     });
+// }
 
 });
--- resources/views/officialpages/partners.blade.php
+++ resources/views/officialpages/partners.blade.php
@@ -13,7 +13,7 @@
 
     <section class="partnerslogsdisplay">
             @foreach ($company_names as $company_name)
-                @if($company_name->company_logo_path != "delipack1.png")
+                @if($company_name->company_logo_path != "delipack1")
                     <div> 
                         <img src="https://superuser.delipackport.com/company_logos/{{$company_name->company_logo_path}}" />
                     </div>
